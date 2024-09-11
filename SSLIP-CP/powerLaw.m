75660bc970a78cbca30133ab29025dfac02562a9
Dorian <dorian.dep@gmail.com>
2024-09-11 10:34:07 +0200

    Remove duplicates

--- SSLIP-CP/powerLaw.m
+++
@@ -1,7 +0,0 @@
-function h = powerLaw(h0, CRSS, latent_matrix, satu_stress, a)
-	n_ss=length(CRSS);
-    k=zeros(size(CRSS));
-    k(CRSS<satu_stress)=h0.*(1-CRSS(CRSS<satu_stress)./satu_stress).^a;
-	h=repmat(k',[n_ss 1]).*latent_matrix;
-end
-
